f81fec6517d7e5e72334a909cb44dccd0386431a
koffaJeanAgoudavi-dev <jeanagoudavi890@gmail.com>
2026-09-14 12:10:43 +0000

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
 from app.core.config import settings
+from app.modules.telegram.router import router as telegram_router
 
 app = FastAPI(
     title=settings.APP_NAME,
     description="Backend API pour SmartReply Agent SaaS",
     version="1.0.0"
 )
+
+# Inclusion des routes du module Telegram
+app.include_router(telegram_router)
 
 @app.get("/")
 def read_root():
