52be1c0fef32ae695c63bca611ffc0a41e27c1c0
koffaJeanAgoudavi-dev <jeanagoudavi890@gmail.com>
2026-09-18 19:59:34 +0000

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -24,6 +24,9 @@ class Settings(BaseSettings):
     GOOGLE_CLIENT_ID: str = os.getenv("GOOGLE_CLIENT_ID", "")
     GOOGLE_CLIENT_SECRET: str = os.getenv("GOOGLE_CLIENT_SECRET", "")
     GOOGLE_REFRESH_TOKEN: str = os.getenv("GOOGLE_REFRESH_TOKEN", "")
+    
+    # Sheet Data Entreprise (Module 4)
+    GOOGLE_DATA_SHEET_ID: str = os.getenv("GOOGLE_DATA_SHEET_ID", "")
 
     class Config:
         env_file = ".env"
