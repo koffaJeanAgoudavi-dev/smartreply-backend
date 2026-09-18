2d1edbe15ab25c4018d3d86d7cb3659a2353198e
koffaJeanAgoudavi-dev <jeanagoudavi890@gmail.com>
2026-09-18 20:56:36 +0000

    Update main.py

--- app/main.py
+++ app/main.py
@@ -6,6 +6,7 @@ from app.modules.gmail.router import router as gmail_router
 from app.modules.filtering.router import router as filtering_router
 from app.modules.sheets.router import router as sheets_router
 from app.modules.drafts.router import router as drafts_router
+from app.modules.notifications.router import router as notifications_router
 
 app = FastAPI(
     title=settings.APP_NAME,
@@ -20,6 +21,7 @@ app.include_router(gmail_router)
 app.include_router(filtering_router)
 app.include_router(sheets_router)
 app.include_router(drafts_router)
+app.include_router(notifications_router)
 
 
 @app.get("/")
